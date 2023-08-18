c34994c49eabfcfaa7f8fda7f18c5ffc514b6ef1
KGCybeX <k4gcybex@gmail.com>
2023-08-18 22:26:08 +0200

    feat: updated intents to GatewayIntentBits

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('node:path');
 const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
 const {loadCommands} = require("./utils/load-slashes");
 
-const client = new Client({ intents: intentList });
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]});
 client.commands = new Collection();
 
 const commandFolderPath = path.join(__dirname, "commands");
